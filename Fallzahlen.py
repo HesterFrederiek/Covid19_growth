2fe70193c1cb071744342e2c5e0b429784832fdd
HesterFrederiek <hesterpieters@gmail.com>
2021-09-13 11:45:17 +0200

    Cleaned up

--- Fallzahlen.py
+++ Fallzahlen.py
@@ -5,38 +5,21 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 df_Fallzahlen = pd.read_csv('Fallzahlen_BS.csv', sep=';')
-print(df_Fallzahlen)
-print(df_Fallzahlen.columns)
-print(df_Fallzahlen['Datum'])
-
-dates = df_Fallzahlen['Datum']
-x = [dt.datetime.strptime(d, '%Y-%m-%d') for d in dates]
-print(x)
 
 
-days = mdates.drange(min(x), max(x) + dt.timedelta(days=1), dt.timedelta(days=1))
-
-
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=40))
-plt.bar(x, df_Fallzahlen['Differenz Fälle mit Wohnsitz BS'])
-plt.gcf().autofmt_xdate()
-
-#plt.show()
-plt.close()
 
 #Compare with estimates
 df_Fallzahlen_reduced = df_Fallzahlen[['Differenz Fälle mit Wohnsitz BS', 'Datum']]
 df_estimates = pd.read_csv('verdoppelung.csv')
+df_estimates_BS = df_estimates[df_estimates['Region'] == 'BS']
+df_combined = df_estimates_BS.merge(df_Fallzahlen_reduced, how='left', on='Datum')
 
-df_combined = df_estimates.merge(df_Fallzahlen_reduced, how='left', on='Datum')
 
-print(df_combined)
-print(df_combined.columns)
+
 
 dates = df_combined['Datum']
 x = [dt.datetime.strptime(d, '%Y-%m-%d') for d in dates]
-print(x)
+
 
 
 days = mdates.drange(min(x), max(x) + dt.timedelta(days=1), dt.timedelta(days=1))
@@ -47,6 +30,9 @@ plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=40))
 plt.bar(x, df_combined['Differenz Fälle mit Wohnsitz BS'])
 plt.gcf().autofmt_xdate()
 
-plt.bar(x, df_combined['Mittlere growth rate'] * 100, color='r')
-
 plt.show()
+
+#plt.plot(df_combined['Mittlere growth rate'])
+
+
+
